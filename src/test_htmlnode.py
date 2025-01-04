fb5e98b60a6545c317b2b645d7740875e560cf7d
Ramon Tanoeiro <ramon.tanoeiro@indebted.co>
2025-01-04 21:35:02 +1100

    added tests to check behaviour for leafnodes

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLtNode(unittest.TestCase):
@@ -20,3 +20,24 @@ class TestHTMLtNode(unittest.TestCase):
         self.assertEqual(node.value, "This is some text")
         self.assertEqual(node.children, None)
         self.assertEqual(node.props, None)
+
+    def test_leaf_raise_error(self):
+        node = LeafNode(tag="a", value=None, props={"href": "https://www.google.com"})
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_leaf_node_build(self):
+        node = LeafNode(
+            tag="a", value="Google Website", props={"href": "https://www.google.com"}
+        )
+        self.assertEqual(
+            node.to_html(), '<a href="https://www.google.com">Google Website</a>'
+        )
+
+    def test_to_html_no_children(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
