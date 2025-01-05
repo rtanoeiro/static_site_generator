55ff687978b440672658dc7051558f684e082523
Ramon Tanoeiro <ramon.tanoeiro@indebted.co>
2025-01-05 17:22:26 +1100

    modified test names to be more appropriate

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -42,67 +42,62 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertNotEqual(node, node2)
 
-    def test_text_html_node(self):
+    def test_text_node_to_html_node_text(self):
         text_node = TextNode("This is a test text node", TextType.TEXT)
         html_node = text_node_to_html_node(text_node)
         self.assertEqual(html_node.value, "This is a test text node")
 
-    def test_text_html_node_building(self):
-        text_node = TextNode("This is a test text node", TextType.TEXT)
-        html_node = text_node_to_html_node(text_node).to_html()
-        self.assertEqual(html_node, "This is a test text node")
-
-    def test_bold_html_node(self):
+    def test_text_node_to_html_node_bold(self):
         text_node = TextNode("This is a test bold text", TextType.BOLD)
         html_node = text_node_to_html_node(text_node)
         self.assertEqual(html_node.tag, "b")
         self.assertEqual(html_node.value, "This is a test bold text")
 
-    def test_bold_html_node_building(self):
+    def test_text_node_to_html_node_bold_to_html(self):
         text_node = TextNode("This is a test bold text", TextType.BOLD)
         html_node = text_node_to_html_node(text_node).to_html()
         self.assertEqual(html_node, "<b>This is a test bold text</b>")
 
-    def test_italic_html_node(self):
+    def test_text_node_to_html_node_italic(self):
         text_node = TextNode("This is a test italic text", TextType.ITALIC)
         html_node = text_node_to_html_node(text_node)
         self.assertEqual(html_node.tag, "i")
         self.assertEqual(html_node.value, "This is a test italic text")
 
-    def test_italic_html_node_building(self):
+    def test_text_node_to_html_node_italic_to_html(self):
         text_node = TextNode("This is a test italic text", TextType.ITALIC)
         html_node = text_node_to_html_node(text_node).to_html()
         self.assertEqual(html_node, "<i>This is a test italic text</i>")
 
-    def test_code_html_node(self):
+    def test_text_node_to_html_node_code(self):
         text_node = TextNode("This is a test code", TextType.CODE)
         html_node = text_node_to_html_node(text_node)
         self.assertEqual(html_node.tag, "code")
         self.assertEqual(html_node.value, "This is a test code")
 
-    def test_code_html_node_building(self):
+    def test_text_node_to_html_node_code_to_html(self):
         text_node = TextNode("This is a test code", TextType.CODE)
         html_node = text_node_to_html_node(text_node).to_html()
         self.assertEqual(html_node, "<code>This is a test code</code>")
 
-    def test_link_html_node(self):
+    def test_text_node_to_html_node_link(self):
         text_node = TextNode("This is a test link", TextType.LINK)
         html_node = text_node_to_html_node(text_node)
         self.assertEqual(html_node.tag, "a")
         self.assertEqual(html_node.value, "This is a test link")
 
-    def test_link_html_node_building(self):
+    def test_text_node_to_html_node_link_to_html(self):
         text_node = TextNode("This is a test link", TextType.LINK, url="www.google.com")
         html_node = text_node_to_html_node(text_node).to_html()
         self.assertEqual(html_node, '<a href="www.google.com">This is a test link</a>')
 
-    def test_image_html_node(self):
+    def test_text_node_to_html_node_image(self):
         text_node = TextNode("This is a test image", TextType.IMAGE)
         html_node = text_node_to_html_node(text_node)
         self.assertEqual(html_node.tag, "img")
         self.assertEqual(html_node.value, "")
 
-    def test_image_html_node_building(self):
+    def test_text_node_to_html_node_image_to_html(self):
         text_node = TextNode(
             "This is a test image", TextType.IMAGE, url="www.google.com"
         )
